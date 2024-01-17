b9d9cb5733e8b06ab526073819cf745a10bcd869
pavlos tzitzos <94289294+PavlosTzitzos@users.noreply.github.com>
2024-01-17 18:52:57 +0200

    Delete undergroundwebsite-vsc directory

--- undergroundwebsite-vsc/undergroundwebsite-old/sql/test_inserts.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO SHOW_TABLE(`SHOW_NAME`, `LOGO_PATH`, `PRODUCER_1`, `DESCRIPTION`) VALUES ('Underground Radio', 'Test', 'Auto DJ', 'Hello I\'m the player of Underground Radio station. I play music when there is no show on air.');
-
-INSERT INTO SHOW_TABLE(`SHOW_NAME`, `LOGO_PATH`, `PRODUCER_1`, `PRODUCER_2`, `DESCRIPTION`) VALUES ('Post Covid Show', 'Test', 'Kostas', 'Thanasis', 'Είμαστε ο Θανάσης και ο Κώστας, Ηλεκτρολόγοι Μηχανικοί. Στην εκπομπή μας θα συζητάμε διάφορα θέματα ενώ στο ενδιάμεσο θα υπάρχουν μουσικά διαλλείμματα με μουσική από πολλά διαφορετικά είδη.');
-
-INSERT INTO SHOW_TABLE(`SHOW_NAME`, `LOGO_PATH`, `PRODUCER_1`, `DESCRIPTION`) VALUES ('X METAL ZONE', 'Test', 'Γιώργος', 'Σκληρός ήχος');
-
-INSERT INTO SHOW_TABLE(`SHOW_NAME`, `LOGO_PATH`, `PRODUCER_1`, `PRODUCER_2`, `DESCRIPTION`) VALUES ('Καφές & Υπομονή', 'Test', 'Άννα', 'Κώστας', 'Σκληρός ήχος');
